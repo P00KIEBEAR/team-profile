cdd8adeb05ea446672ed796a47bfb36da9e3101d
pat paggi <patpaggi@gmail.com>
2020-11-24 11:27:33 -0700

    html

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,23 +1,27 @@
+
 const Employee = require('../lib/Employee');
 
-test('creates a employee object', () => {
-  const employee = new Employee('Pat');
-  //should be seperate tests
-  expect(employee.name).toBe('Dave');
-  //
-  expect(employee.id).toEqual(expect.any(Number));
-  //
-  expect(employee.email).toBe('pat@gmail.com');
-});
-test("gets employee's stats as an object", () => {
-  const employee = new Employee('Pat');
 
-  expect(employee.getName()).toHaveProperty('name');
+describe('Person', () =>
+  it('should be a name', () => {
+    const employee = new Employee('Pat');
+    //should be seperate tests
+    expect(employee.name).toBe('Dave');
+  }),
+  it('should be id', () => {
+    expect(employee.id).toEqual(expect.any(Number));
+    //
+    expect(employee.email).toBe('pat@gmail.com');
+  }),
+  test("gets employee's stats as an object", () => {
+    const employee = new Employee('Pat');
 
-  expect(employee.getId()).toHaveProperty('id');
+    expect(employee.getName()).toHaveProperty('name');
 
-  expect(employee.getEmail()).toHaveProperty('email');
+    expect(employee.getId()).toHaveProperty('id');
 
-});
+    expect(employee.getEmail()).toHaveProperty('email');
+
+  }));
 
 
