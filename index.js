0f7d6a8b97603112fd6c30ec7254c0085e32d900
pat paggi <patpaggi@gmail.com>
2020-11-27 11:47:01 -0700

    so lost

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
 const Employees = require('./lib/Team');
+const fs = require('fs');
+const pageHTML = require('http')
 
+Employees = getEmployees().then(() => {
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Page created! Check out index.html in this directory to see it!');
 
-Employees = getEmployees();
+    fs.copyFile('../src/style.css', '../dist/style.css', err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Style sheet copied successfully!');
+    });
+  });
+})
--- src/page-template.js
+++ src/page-template.js
@@ -3,36 +3,36 @@ const Employees = require("../lib/Team");
 
 
 function generatePage(Employees) {
-  // console.log(Employees)
-  `
- <!DOCTYPE html>
- <html lang="en">
- 
- <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-   <link rel="stylesheet" href="./dist/style.css" />
-   <title>MY TEAM</title>
- </head>
- 
-   <body>
-      <header class="hero">
-     <h1 class="app-title">My Team</h1>
-     
-   </header>
-      <main class="flex-row justify-space-between">
-      <div class="container m-0">
-           <div class="row card-5 " id="team">
-           </div>
-      </div>
- 
-     </main>
-    <script src="/.dist/script.js"></script>
-   </body>
-   </html>
-       
-   `
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./dist/style.css" />
+    <title>MY TEAM</title>
+  </head>
+  
+    <body>
+       <header class="hero">
+      <h1 class="app-title">My Team</h1>
+      
+    </header>
+       <main class="flex-row justify-space-between">
+       <div class="container m-0">
+            <div class="row card-5 " id="team">
+            </div>
+       </div>
+  
+      </main>
+     <script src="/.dist/script.js"></script>
+    </body>
+    </html>
+        
+    `
 };
 
 module.exports = generatePage;
