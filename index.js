a4769352dc8e57c3e87ca4aad24b2db886e0346f
pat paggi <patpaggi@gmail.com>
2020-11-28 22:43:37 -0700

    done

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 
-const Employees = require('./lib/Team');
+//const Employees = require('./lib/Team');
 const fs = require('fs');
 const pageHTML = require('http')
+const team = require('./lib/Team')
 
-Employees = getEmployees().then(() => {
-  fs.writeFile('./index.html', pageHTML, err => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('Page created! Check out index.html in this directory to see it!');
 
-    fs.copyFile('../src/style.css', '../dist/style.css', err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Style sheet copied successfully!');
-    });
-  });
-})
+
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,8 +1,53 @@
-const Employees = require("../lib/Team");
+const Employee = require("../lib/Employee")
+const Manager = require("../lib/Manager");
+const Intern = require('../lib/Intern');
+const Engineer = require('../lib/Engineer')
 
 
-
+// 
 function generatePage(Employees) {
+  //const data = JSON.stringify(Employees)
+  //console.log(Employees);
+  const managers = Employees.filter(Employee => Employee.position === 'Manager');
+  const engineers = Employees.filter(Employee => Employee.position === 'Engineer');
+  const interns = Employees.filter(Employee => Employee.position === 'Intern');
+
+  const internForm = interns.map(internForm => {
+    console.log(internForm.getSchool())
+    internForm = `<div class="col-2 mx-auto">
+  <h3>${internForm.getPosition()}</h3>
+  <p>${internForm.getName()}</p>
+  <p>${internForm.getId()}</p>
+   <a href="mailto:${internForm.getEmail()}"> Email: ${internForm.getEmail()}</a>
+   <p>${internForm.getSchool()}<p>
+   </div>
+   `
+    return internForm
+  })
+  const engineerForm = engineers.map(engineerForm => {
+
+    engineerForm = `<div class="col-2 mx-auto">
+  <h3>${engineerForm.getPosition()}</h3>
+  <p>${engineerForm.getName()}</p>
+  <p>${engineerForm.getId()}</p>
+   <a href="mailto:${engineerForm.getEmail()}"> Email: ${engineerForm.getEmail()}</a>
+      <p>${engineerForm.getGithub()}<p>
+      </div>
+   `
+    return engineerForm
+  })
+  const managerForm = managers.map(managerForm => {
+
+    managerForm = `<div class="col-2 mx-auto">
+  <h3>${managerForm.getPosition()}</h3>
+  <p>${managerForm.getName()}</p>
+  <p>${managerForm.getId()}</p>
+   <a href="mailto:${managerForm.getEmail()}"> Email: ${managerForm.getEmail()}</a>
+      <p>${managerForm.getOfficeNumber()}<p>
+   </div>
+   `
+    return managerForm
+  })
 
   return `
   <!DOCTYPE html>
@@ -24,9 +69,13 @@ function generatePage(Employees) {
        <main class="flex-row justify-space-between">
        <div class="container m-0">
             <div class="row card-5 " id="team">
+            ${managerForm}
+            ${engineerForm}
+            ${internForm}
             </div>
        </div>
-  
+ 
+ 
       </main>
      <script src="/.dist/script.js"></script>
     </body>
@@ -34,5 +83,5 @@ function generatePage(Employees) {
         
     `
 };
-
+//${Employees.length ? '* ' + (Employees) : ''}
 module.exports = generatePage;
